80016e93ca6f9cc2509797511aa4bc2e44c669a4
Pierce Cirks <97271154+FiercelyPierce@users.noreply.github.com>
2023-07-07 18:38:26 -0700

    Started but unsure

--- messenger/control.py
+++ messenger/control.py
@@ -2,9 +2,20 @@
 # COMPONENT:
 #    CONTROL
 # Author:
-#    Br. Helfrich, Kyle Mueller, <your name here if you made a change>
+#    Br. Helfrich, Kyle Mueller, Pierce Cirks, <your name here if you made a change>
 # Summary: 
 #    This class stores the notion of Bell-LaPadula
 ########################################################################
+from enum import Enum
 
-# you may need to put something here...
+class Control(Enum):
+    Public = 1
+    Confidential = 2
+    Privileged = 3
+    Secret = 4
+
+    def authenticate(self,username,password):
+        pass
+
+    def securityConditionRead(self, assetControl, subjectControl):
+        pass
