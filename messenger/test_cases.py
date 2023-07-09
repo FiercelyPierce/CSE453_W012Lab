2c48a2bf2ee953d5c9a487de6530105af9cf8e50
Bryer Johnson <bryer808@gmail.com>
2023-07-08 20:23:56 -0600

    double check this please!

--- messenger/test_cases.py
+++ messenger/test_cases.py
@@ -1,7 +1,18 @@
+import self
 import unittest
 import interact
 import messages
 import control
+
+
+########################################################################
+# COMPONENT:
+#    test_cases
+# Author:
+#    Bryer Johnson
+# Summary: 
+#    Test cases
+########################################################################
 
 
 class Interact(unittest.TestCase):
@@ -26,6 +37,54 @@ class Interact(unittest.TestCase):
         expected_output = "[5] Message from CaptainCharlie at [date]\n\tMessage: [message]"
         self.assertIn(expected_output, interact_.show(control.Control("CaptainCharlie", "password", self.messages_)))
         
+    def can_read_and_can_write(self):
+    
+    messages_ = messages.Messages("messages.txt")
+    
+        
+    """ 
+    User with Privileged access
+    """
+    control_privileged = control.Control("CaptainCharlie", "password", messages_)
+    self.assertTrue(control_privileged.can_read(["1", "Author", "Date", "Public"]))
+    self.assertTrue(control_privileged.can_read(["2", "Author", "Date", "Confidential"]))
+    self.assertTrue(control_privileged.can_read(["3", "Author", "Date", "Privileged"]))
+    self.assertFalse(control_privileged.can_read(["4", "Author", "Date", "Secret"]))
+
+    self.assertTrue(control_privileged.can_write(["1", "Author", "Date", "Public"]))
+    self.assertFalse(control_privileged.can_write(["2", "Author", "Date", "Confidential"]))
+    self.assertTrue(control_privileged.can_write(["3", "Author", "Date", "Privileged"]))
+    self.assertTrue(control_privileged.can_write(["4", "Author", "Date", "Secret"]))
+
+    """
+    User with Confidential access
+    """
+    
+    control_confidential = control.Control("SeamanSam", "password", messages_)
+    self.assertTrue(control_confidential.can_read(["1", "Author", "Date", "Public"]))
+    self.assertTrue(control_confidential.can_read(["2", "Author", "Date", "Confidential"]))
+    self.assertFalse(control_confidential.can_read(["3", "Author", "Date", "Privileged"]))
+    self.assertFalse(control_confidential.can_read(["4", "Author", "Date", "Secret"]))
+
+    self.assertTrue(control_confidential.can_write(["1", "Author", "Date", "Public"]))
+    self.assertTrue(control_confidential.can_write(["2", "Author", "Date", "Confidential"]))
+    self.assertFalse(control_confidential.can_write(["3", "Author", "Date", "Privileged"]))
+    self.assertFalse(control_confidential.can_write(["4", "Author", "Date", "Secret"]))
+
+    """
+    User with Secret access
+    """
+    control_secret = control.Control("AdmiralAbe", "password", messages_)
+    self.assertTrue(control_secret.can_read(["1", "Author", "Date", "Public"]))
+    self.assertTrue(control_secret.can_read(["2", "Author", "Date", "Confidential"]))
+    self.assertTrue(control_secret.can_read(["3", "Author", "Date", "Privileged"]))
+    self.assertTrue(control_secret.can_read(["4", "Author", "Date", "Secret"]))
+
+    self.assertTrue(control_secret.can_write(["1", "Author", "Date", "Public"]))
+    self.assertTrue(control_secret.can_write(["2", "Author", "Date", "Confidential"]))
+    self.assertTrue(control_secret.can_write(["3", "Author", "Date", "Privileged"]))
+    self.assertFalse(control_secret.can_write(["4", "Author", "Date", "Secret"]))
+
         
 
 if __name__ == '__main__':
